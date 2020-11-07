c07ef83577b061c13de30a6f179b8b509ceeb822
Corentin-pcn <pichoncorentin@gmail.com>
2020-11-07 11:41:12 +0100

    Finished A and B

--- TP_1.py
+++ TP_1.py
@@ -12,7 +12,8 @@ r = 287.053
 rho = 0.568607 #kg/m3
 P = 39271 #Pa
 T = 240.6 #K
-c = np.sqrt(gamma*r*T) #m/s
+vs = np.sqrt(gamma*r*T) #m/s
+g0 = 9.81 #m/s2
 
 #Plane specification
 S = 34 #m2
@@ -24,7 +25,7 @@ c = 0.52 #centering
 
 #Flight parameters
 M = 1.4
-Veq = c*M #m/s
+Veq = vs*M #m/s
 h = 24000 #ft
 Czdm = 0.59
 dm0 = 0
@@ -36,10 +37,53 @@ Cza = 2.58
 a0 = 0.008
 f = 0.608
 
-Czeq = 0.0449
+Czeq = 0.0439
+gammaeq = 0
+aeq = 0.030
+Feq = 62916.1 #N
 
 Q = (1/2)*rho*(Veq**2) 
 Cxeq = Cx0 + k*(Czeq**2)
+Cxa = Cx0 + k*(Cza**2)
+Cxdm = Cx0 + k*(Czdm**2)
+Iyy = m*(rg**2)
+
+X = f*lt - c*lt
+Y = fd*lt - c*lt
+
+Cma = (X/lref)*(Cxa*np.sin(aeq) + Cza*np.cos(aeq))
+Cmdm = (Y/lref)*(Cxdm*np.sin(aeq) + Czdm*np.cos(aeq))
 
 Xv = (2*Q*S*Cxeq)/(m*Veq)
 Zv = (2*Q*S*Czeq)/(m*Veq)
+Xg = (g0*np.cos(gammaeq))/(Veq)
+Xa = (Feq*np.sin(aeq)+(Q*S*Cxa))/(m*Veq)
+Za = (Feq*np.cos(aeq)+(Q*S*Cza))/(m*Veq)
+ma = (Q*S*lref*Cma)/(Iyy)
+mq = (Q*S*lref*Cmq)/(Iyy)
+Zdm = (Q*S*Czdm)/(m*Veq)
+mdm = (Q*S*lref*Cmdm)/(Iyy)
+
+A = np.zeros([6,6])
+B = np.zeros([6,1])
+
+A[0,0] = -Xv
+A[0,1] = -Xg
+A[0,2] = -Xa
+A[1,0] = Zv
+A[1,2] = Za
+A[2,0] = -Zv
+A[2,2] = -Za
+A[2,3] = 1
+A[3,2] = ma
+A[3,3] = mq
+A[4,3] = 1
+A[5,1] = Veq
+
+B[1] = Zdm
+B[2] = -Zdm
+B[3] = mdm
+
+np.set_printoptions(4,suppress=True)
+print(A)
+print(B)
